0097d07bd3d163e758871c5971535f7bf9d78b15
Medic <71578066+MedicDoesStuff@users.noreply.github.com>
2024-02-15 18:16:59 +0800

    Update run_script.py

--- run_script.py
+++ run_script.py
@@ -1,16 +1,17 @@
-# Start Applio
-
 import codecs
 import os
 import subprocess
 
 def main():
+    # Change the directory to /content/VoiceGenAI/program/
+    os.chdir('/content/VoiceGenAI/program/')
+
     applio_script = codecs.decode("ncc.cl", "rot_13")
 
     # Start the TensorBoard server in the background with subprocess
     tensorboard_process = subprocess.Popen(['tensorboard', '--logdir', 'logs', '--bind_all'])
 
-    # Run the Applio script
+    # Assuming that the decoded applio_script path is relative to the new current directory
     subprocess.call(['python', applio_script, '--share'])
 
     # Keep the script running to allow TensorBoard and Applio to continue running
